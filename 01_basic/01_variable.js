d3703cd915db0b3a6b9466681550676c0775f1e3
SANJEEV <159331593+SANJEEVQWERTY@users.noreply.github.com>
2024-05-23 21:21:11 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 12345
+let accountName = "Sanjeev"
+let accountEmail = "supercomputer197@gmail.com"
+var accountPassword = "5555555"
+accountCity = "Jaunpur"
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity])
