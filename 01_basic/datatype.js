d78ef4d8e0ecc3de3eb806a66c8167ee62154885
SANJEEV <159331593+SANJEEVQWERTY@users.noreply.github.com>
2024-05-24 17:24:08 +0000

    commit message

---
+++ 01_basic/datatype.js
@@ -0,0 +1,24 @@
+"use strict";     // all javascript code in newer version.
+ 
+//alert(hello)
+
+//console.log(54+23)      // code readebility is very high priority
+
+//*****************Datatype**************************
+  
+
+// number => o to 2 power 52
+// string =>   ""
+// boolean =>  true/false
+// null =>  standlong value 
+// undefined => 
+// symbol => unique
+
+
+let name = "Sanjeev"
+let age = 24
+let bool = true
+console.log(typeof "name")
+console.log(typeof 24)
+console.log(typeof bool)
+console.log(typeof null)
