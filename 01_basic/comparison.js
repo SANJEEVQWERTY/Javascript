43454e0d113040c928e9227c30538d20382b58da
SANJEEV <159331593+SANJEEVQWERTY@users.noreply.github.com>
2024-05-24 23:14:32 +0000

    complete comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,19 @@
+console.log(3>4)              // output => false
+console.log(3>2)              // output => true
+
+console.log(2>=1,2==1,2!=1,2<=1)          // output => true false true false
+
+console.log("1">2)                  // output =>false
+
+console.log(null>0)                  // output => false
+console.log(null<=0)                  // output => true
+console.log(null>=0)                 // output => true
+
+console.log(undefined == 0)           // output => false
+console.log(undefined <=0)            // output => false
+console.log(undefined >=0)            // output => false
+console.log(undefined>0)              // output => false
+
+console.log("2" === 2)                 // output => false
+console.log(3 === 3)                   // output => true
+/*  (===) Check value as well as datatype */
