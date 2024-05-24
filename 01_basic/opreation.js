110104766305386eedafb0320406a6485cef3b98
SANJEEV <159331593+SANJEEVQWERTY@users.noreply.github.com>
2024-05-24 22:49:29 +0000

    opreation is done

---
+++ 01_basic/opreation.js
@@ -0,0 +1,23 @@
+let value = 2
+let changeSign = -value
+console.log(changeSign)         // output => -2
+let changeType = String(value)
+console.log(typeof changeType)    // output => string
+console.log(2+2,2*4,4/1,5%3,3**4)      // output => 4 8 4 2 81
+ 
+let str1 = "Sanjeev "
+let str2 = "Yadav"
+let str3 = str1 + str2
+console.log(str3)                // output  => Sanjeev Yadav
+
+
+console.log("1"+2)               // output  => 12
+console.log(1+"2")               // output  => 12
+console.log("1"+"2")             // output  => 12
+console.log(1+2+3+"4")           // output  => 64
+console.log("1"+2+3);            // output  => 123
+
+let counter = 100
+++counter
+console.log(counter)                // output  => 101
+console.log(typeof counter)          // output  => number
